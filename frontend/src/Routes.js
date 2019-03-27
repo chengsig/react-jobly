21de0a1b2a6d8d7ef4a41404e729d766ed4cbb27
annika <annikaslund@gmail.com>
2019-03-27 11:04:49 -0700

    Add skeleton files

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,2 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import Nav from './Nav';
+import Homepage from './Homepage';
+import Companies from './Companies';
+import Company from './Company';
+import Jobs from './Jobs';
+import Login from './Login';
+import Profile from './Profile';
+
+class Routes extends Component {
+    render(){
+        return (
+            <BrowserRouter>
+                <Nav />
+                <Switch>
+                    <Route exact path="/" render={ () => <Homepage /> }/>
+                    <Route exact path="/companies" render={ () => <Companies /> }/>
+                    <Route exact path="/companies/:handle" render={ (rtProps) => <Company { ...rtProps }/> }/>
+                    <Route exact path="/jobs" render={ () => <Jobs /> }/>
+                    <Route exact path="/login" render={ () => <Login /> }/>
+                    <Route exact path="/profile" render={ () => <Profile /> }/>
+                    <Redirect to="/"/>
+                </Switch>
+            </BrowserRouter>
+        );
+    }   
+}
+
+export default Routes;
