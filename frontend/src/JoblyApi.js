cdf84913e3205ab282a47edddcb8aca2e34d4dc9
annika <annikaslund@gmail.com>
2019-03-27 10:53:32 -0700

    Add JoblyApi class

---
+++ frontend/src/JoblyApi.js
@@ -0,0 +1,31 @@
+class JoblyApi {
+    static async request(endpoint, paramsOrData = {}, verb = "get") {
+      paramsOrData._token = ( // for now, hardcode token for "testuser"
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6" +
+      "InRlc3R1c2VyIiwiaXNfYWRtaW4iOmZhbHNlLCJpYXQiOjE1NDE1N" +
+      "jQ2Nzl9.LYDHSkl81gEm7jfHv9wJhzD4ndpuBkSzBan8Nirb6UY");
+  
+      console.debug("API Call:", endpoint, paramsOrData, verb);
+  
+      try {
+        return (await axios({
+          method: verb,
+          url: `http://localhost:3001/${endpoint}`,
+          [verb === "get" ? "params" : "data"]: paramsOrData})).data;
+          // axios sends query string data via the "params" key,
+          // and request body data via the "data" key,
+          // so the key we need depends on the HTTP verb
+      }
+  
+      catch(err) {
+        console.error("API Error:", err.response);
+        let message = err.response.data.message;
+        throw Array.isArray(message) ? message : [message];
+      }
+    }
+  
+    static async getCompany(handle) {
+      let res = await this.request(`companies/${handle}`);
+      return res.company;
+    }
+  }
