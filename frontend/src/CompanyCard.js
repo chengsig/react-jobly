eeafd62630b00520a3f1fc9367883412911225cb
annika <annikaslund@gmail.com>
2019-03-27 17:02:53 -0700

    Add styling

--- frontend/src/CompanyCard.js
+++ frontend/src/CompanyCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-//import "./CompanyCard.css";
+import "./CompanyCard.css";
 
 class CompanyCard extends Component {
   render() {
--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-//import './Nav.css';
+import './Nav.css';
 
 class Nav extends Component {
     render(){
