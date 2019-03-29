9d2fda2d19f2077d8f42581b1d68c68abb50d56c
Chengsi <gaochengsi@gmail.com>
2019-03-29 16:34:27 -0700

    cleaning up commented token logic to refresh env variable

--- frontend/src/JoblyApi.js
+++ frontend/src/JoblyApi.js
@@ -2,12 +2,8 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001/";
 class JoblyApi {
     static async request(endpoint, paramsOrData = {}, verb = "get") {
-      paramsOrData._token = ( // for now, hardcode token for "testuser"
-      // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6" +
-      // "InRlc3R1c2VyIiwiaXNfYWRtaW4iOmZhbHNlLCJpYXQiOjE1NDE1N" +
-      // "jQ2Nzl9.LYDHSkl81gEm7jfHv9wJhzD4ndpuBkSzBan8Nirb6UY"
-      localStorage.getItem("_token")
-      );
+      
+      paramsOrData._token = localStorage.getItem("_token")
   
       console.debug("API Call:", endpoint, paramsOrData, verb);
   
